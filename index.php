4e11305957d30d42afa2daae9416ccbdbd68fdc5
syruxst <daniel@ugalde.cl>
2024-03-03 15:14:24 -0300

    hola

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require('admin/conex.php');?>
 <!DOCTYPE html>
 <html lang="es">
+    <!-- esto es una prueba-->
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
